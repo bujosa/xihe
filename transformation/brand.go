3d3b198f2f111b6bc9f8f5a211e195c2c29cbfa6
bujosa <davidbujosa@gmail.com>
2023-07-05 13:17:22 -0400

    feat: add brand serialization for cars

---
+++ transformation/brand.go
@@ -0,0 +1,85 @@
+package transformation
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bujosa/xihe/env"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const DATABASE = "supercarros"
+const COLLECTION = "cars"
+const SOURCE = "brands"
+const DB_URL_ENV_KEY = "SUPER_CARROS_DATABASE_URL"
+
+func Brand() {
+	dbUri, err := env.GetString(DB_URL_ENV_KEY)
+	print(dbUri)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set up the client and connect to the database.
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(
+		dbUri,
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the database and collection.
+	db := client.Database(DATABASE)
+	coll := db.Collection(COLLECTION)
+
+	// Define a filter that matches all documents in the collection.
+	pipeline := []bson.M{
+		{
+			"$addFields": bson.M{
+				"brandSlug": bson.M{
+					"$toLower": "$brand",
+				},
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from": SOURCE,
+				"localField": "brandSlug",
+				"foreignField": "slug",
+				"as": "result",
+			},
+		},
+		{
+			"$unwind": bson.M{
+				"path": "$result",
+				"preserveNullAndEmptyArrays": true,
+			},
+		},
+		{
+			"$addFields": bson.M{
+				"brandId": bson.M{
+					"$ifNull": []interface{}{"$result._id", nil},
+				},
+			},
+		},
+		{
+			"$out": "results",
+		},
+	}
+	// Execute the aggregation.
+	cursor, err := coll.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		panic(err)
+	}
+
+	for cursor.Next(context.TODO()) {
+		var doc bson.D
+		err := cursor.Decode(&doc)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Operation successful.")
+	}
+}
