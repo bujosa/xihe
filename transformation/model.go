7f2219209f6e0ab77b147840793b2ddfcb8a057c
bujosa <davidbujosa@gmail.com>
2023-07-05 17:04:13 -0400

    feat: implement model transformation

--- transformation/model.go
+++ transformation/model.go
@@ -1,5 +1,92 @@
 package transformation
 
-func Model() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/bujosa/xihe/env"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const MODEL_SOURCE = "models"
+const PROCESSED_DATA = "cars_processed"
+
+func Model() {
+	dbUri, err := env.GetString(DB_URL_ENV_KEY)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set up the client and connect to the database.
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(
+		dbUri,
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the database and collection.
+	db := client.Database(DATABASE)
+	coll := db.Collection(PROCESSED_DATA)
+
+	// Define a filter that matches all documents in the collection.
+	pipeline := []bson.M{
+		{
+			"$addFields": bson.M{
+				"model": bson.M{
+					"$concat": []interface{}{
+						"$brand.slug",
+						"-",
+						bson.M{
+							"$replaceAll": bson.M{
+									"input": "$model",
+									"find": " ",
+									"replacement": "-",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from": MODEL_SOURCE,
+				"localField": "model",
+				"foreignField": "slug",
+				"as": "model",
+			},
+		},
+		{
+			"$unwind": bson.M{
+				"path": "$model",
+				"preserveNullAndEmptyArrays": true,
+			},
+		},
+		{
+			"$match": bson.M{
+				"model": bson.M{
+					"$exists": true,
+					"$ne": nil,
+				},
+			},
+		},
+		{"$out": PROCESSED_DATA},
+	}
+	// Execute the aggregation.
+	cursor, err := coll.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		panic(err)
+	}
+
+	for cursor.Next(context.TODO()) {
+		var doc bson.D
+		err := cursor.Decode(&doc)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(doc)
+		fmt.Println("Operation successful.")
+	}
 }
