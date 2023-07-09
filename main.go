bd426cc2d58abc90e588abacee2ec1befe9a1a6e
bujosa <davidbujosa@gmail.com>
2023-07-09 04:39:02 -0400

    feat: add commands for pass parameter via console

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/bujosa/xihe/scripts"
+	"github.com/bujosa/xihe/transformation"
 	"github.com/bujosa/xihe/utils"
 )
 
 func main() {
 	utils.SetLogFile("log.txt")
-	// Data Transformation Pipeline
-	// transformation.RunCarTransformation()
+	var transformationCommand string
+	var uploadCommand string
+	var helpFlag bool
 
-	scripts.UploadDealers()
+	flag.BoolVar(&helpFlag, "h", false, "Help")
+	flag.StringVar(&transformationCommand, "t", "", "Transform data")
+	flag.StringVar(&transformationCommand, "transform", "", "Transform data")
+	flag.StringVar(&uploadCommand, "u", "", "Upload data")
 
-	// scripts.TrimMatchingStrategy()
+	flag.Parse()
+
+	if helpFlag {
+		flag.PrintDefaults()
+		return
+	}
+
+	if transformationCommand == "dealers" {
+		transformation.RunDealerTransformation()
+	} else if transformationCommand == "cars" {
+		transformation.RunCarTransformation()
+	}
+
+	if uploadCommand == "dealers" {
+		scripts.UploadDealers()
+	} else if uploadCommand == "cars" {
+		scripts.TrimMatchingStrategy()
+	}
 }
