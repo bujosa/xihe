8795f0a6fdada19e6023ba6fbed1092ed5f854c3
bujosa <davidbujosa@gmail.com>
2023-07-15 21:26:16 -0400

    feat: add create trim input

---
+++ api/trim.go
@@ -0,0 +1,15 @@
+package api
+
+type CreateTrimInput struct {
+	Alias        string   `json:"alias"`
+	Name         string   `json:"name"`
+	Status       string   `json:"status"`
+	Model        string   `json:"carModel"`
+	FuelType     string   `json:"fuelType"`
+	DriveTrain   string   `json:"driveTrain"`
+	Features     []string `json:"features"`
+	BodyStyle    string   `json:"bodyStyle"`
+	Transmission string   `json:"transmission"`
+	Year         int      `json:"year"`
+	Price        int      `json:"price"`
+}
