d58ea24cae9b0df881751a1fbfca9f95c3db97f7
bujosa <davidbujosa@gmail.com>
2023-07-17 00:15:12 -0400

    feat: implement trim and model create logic to the api

--- api/model.go
+++ api/model.go
@@ -1,5 +1,15 @@
 package api
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/bujosa/xihe/utils"
+)
+
 type CreateModelInput struct {
 	Name   string `json:"name"`
 	Brand  string `json:"brand"`
@@ -16,6 +26,66 @@ type CreateModelResponse struct {
 	} `json:"data"`
 }
 
-func CreateModel(createModelInput CreateModelInput) (Model, error) {
-	return Model{}, nil
+func CreateModel(ctx context.Context, createModelInput CreateModelInput) (Model, utils.StatusRequest) {
+	log.Println("Creating model... with Name: " + createModelInput.Name)
+
+	url := ctx.Value(utils.ProductionApiUrl).(string)
+	token := ctx.Value(utils.SessionSecret).(string)
+
+	mutation := `
+		mutation CreateModel($input: CreateModelInput!) {
+			createModel(input: $input) {
+				id
+			}
+		}
+	`
+
+	requestBody, err := json.Marshal(map[string]interface{}{
+		"query": mutation,
+		"variables": map[string]interface{}{
+			"input": createModelInput,
+		},
+	})
+
+	if err != nil {
+		log.Printf("Error marshalling request body %s\n", err)
+		return Model{}, utils.StatusRequest("failed")
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Printf("Error creating HTTP request %s\n", err)
+		return Model{}, utils.StatusRequest("failed")
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending HTTP request %s\n", err)
+		return Model{}, utils.StatusRequest("failed")
+	}
+	defer response.Body.Close()
+
+	var responseData CreateModelResponse
+
+	err = json.NewDecoder(response.Body).Decode(&responseData)
+	if err != nil {
+		log.Printf("Error decoding response body %s\n", err)
+		return Model{}, utils.StatusRequest("failed")
+	}
+
+	if response.StatusCode != 200 {
+		log.Printf("Error status code %d\n", response.StatusCode)
+		log.Printf("Error data response %s\n", response.Body)
+		return Model{}, utils.StatusRequest("failed")
+	} else {
+		if responseData.Data.CreateModel.Id == "" {
+			log.Printf("Error creating model %s\n", err)
+			return Model{}, utils.StatusRequest("failed")
+		}
+		log.Printf("Model created with ID: %s\n", responseData.Data.CreateModel.Id)
+		return responseData.Data.CreateModel, utils.StatusRequest("success")
+	}
 }
--- api/trim.go
+++ api/trim.go
@@ -1,5 +1,15 @@
 package api
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/bujosa/xihe/utils"
+)
+
 type CreateTrimInput struct {
 	Alias        string   `json:"alias"`
 	Name         string   `json:"name"`
@@ -24,7 +34,66 @@ type CreateTrimResponse struct {
 	} `json:"data"`
 }
 
-func CreateTrim(createTrimInput CreateTrimInput) (Trim, error) {
-	// TODO: Implement this
-	return Trim{}, nil
+func CreateTrim(ctx context.Context, createTrimInput CreateTrimInput) (Trim, utils.StatusRequest) {
+	log.Println("Creating model... with Name: " + createTrimInput.Name)
+
+	url := ctx.Value(utils.ProductionApiUrl).(string)
+	token := ctx.Value(utils.SessionSecret).(string)
+
+	mutation := `
+		mutation CreateTrimLevel($input: CreateTrimLevelInput!) {
+			createTrimLevel(input: $input) {
+				id
+			}
+		}
+	`
+
+	requestBody, err := json.Marshal(map[string]interface{}{
+		"query": mutation,
+		"variables": map[string]interface{}{
+			"input": createTrimInput,
+		},
+	})
+
+	if err != nil {
+		log.Printf("Error marshalling request body %s\n", err)
+		return Trim{}, utils.StatusRequest("failed")
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Printf("Error creating HTTP request %s\n", err)
+		return Trim{}, utils.StatusRequest("failed")
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending HTTP request %s\n", err)
+		return Trim{}, utils.StatusRequest("failed")
+	}
+	defer response.Body.Close()
+
+	var responseData CreateTrimResponse
+
+	err = json.NewDecoder(response.Body).Decode(&responseData)
+	if err != nil {
+		log.Printf("Error decoding response body %s\n", err)
+		return Trim{}, utils.StatusRequest("failed")
+	}
+
+	if response.StatusCode != 200 {
+		log.Printf("Error status code %d\n", response.StatusCode)
+		log.Printf("Error data response %s\n", response.Body)
+		return Trim{}, utils.StatusRequest("failed")
+	} else {
+		if responseData.Data.CreateTrim.Id == "" {
+			log.Printf("Error creating Trim %s\n", err)
+			return Trim{}, utils.StatusRequest("failed")
+		}
+		log.Printf("Trim created with ID: %s\n", responseData.Data.CreateTrim.Id)
+		return responseData.Data.CreateTrim, utils.StatusRequest("success")
+	}
 }
